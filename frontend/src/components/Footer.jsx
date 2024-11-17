887be41bdbeef7eea37d38095097f591ccba2ce0
abdullahbabar580914@gmail.com <abdullahbabar580914@gmail.com>
2024-11-17 12:05:15 +0500

    Connect ListingCard to Listing Details page using dynamic routing

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div>Support</div>
+      <div>Community</div>
+      <div>Hosting</div>
+      <div>About</div>
+      <p>© 2024 Airbnb, Inc. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
